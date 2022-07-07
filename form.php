2aec2790f5a85f1ab0209b01f9e5a9bed95f7e0d
Nikita Lumpov <47274926+nikilum@users.noreply.github.com>
2022-07-08 00:12:54 +0500

    Add files via upload

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <?php
 
-$to = 'info@elementautoservice.ru';
+$to = 'info@element-sto.ru';
 
 function getJson()
 {
@@ -16,19 +16,20 @@ function response($text)
 
 $json = getJson();
 
-if (!isset($json['name']) || !isset($json['phone']) || !isset($json['form_name']))
+if (!isset($json['name']) || !isset($json['contact']) || !isset($json['form_info'])) {
     response('Указаны не все данные');
+}
 
 $name = $json['name'];
-$phone = $json['phone'];
+$contact = $json['contact'];
 $comment = $json['comment'] ? $json['comment'] : '';
-$formName = $json['form_name'];
+$formName = $json['form_info'];
 
-if (!trim($name) || !trim($phone) || !trim($formName))
+if (!trim($name) || !trim($contact) || !trim($formName))
     response('Указаны не все данные');
 
 $subject = 'Заявка с сайта ' . $_SERVER['SERVER_NAME'];
-$message = "Форма: {$formName} <br> Имя: {$name} <br> Номер телефона: {$phone} <br> {$comment}";
+$message = "Форма: {$formName} <br> Имя: {$name} <br> Номер телефона: {$contact} <br> {$comment}";
 $headers
     = "Content-type: text/html; charset=utf-8 \r\n"
     . "From: {$to}\r\n"
@@ -37,6 +38,6 @@ $headers
 $isMailSuccess = mail($to, $subject, $message, $headers);
 
 if ($isMailSuccess)
-    response('Ваше сообщение отправлено!');
+    response('Сообщение отправлено!');
 else
     response('Ошибка отправки сообщения. Попробуйте позже');
